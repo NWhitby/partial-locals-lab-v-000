7e67e63b4c78d636344112a720f32de34e90d7f8
NaKia Whitby <whitby.nakia@gmail.com>
2019-08-06 16:21:03 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,11 +14,12 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def self.search(search)
-    if search
-      where("name like ?", "%#{search}%")
+
+  def self.search(arg)
+    if arg.present?
+      self.where('NAME like ?', "%#{arg}%")
     else
-      Student.all
+      self.all
     end
-  end
+  end   
 end
